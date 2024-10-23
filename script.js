66bb562980bf7cfe2da812756d423acb2745f31f
ElvijsCoder <145661396+ElvijsCoder@users.noreply.github.com>
2024-10-23 12:37:39 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.body.addEventListener("pointermove", (e) => {
+    const { currentTarget: el, clientX: x, clientY: y } = e;
+    const { top: t, left: l, width: w, height: h } = el.getBoundingClientRect();
+    el.style.setProperty('--posX', x - l - w / 2);
+    el.style.setProperty('--posY', y - t - h / 2);
+});
 
+// Scroll event to change body class
+window.addEventListener('scroll', () => {
+    document.body.classList.add('scrolled');
+});
